d68a95e83ecb9036c6b8ac99cb3bc871bcf8542c
asaf mashiah <asaf222@gmail.com>
2015-04-04 11:29:11 +0300

    גרסה שנייה

--- src/com/algoTrader/javacourse/Algo_trader_asafmServlet.java
+++ src/com/algoTrader/javacourse/Algo_trader_asafmServlet.java
@@ -7,7 +7,19 @@ import javax.servlet.http.*;
 public class Algo_trader_asafmServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.setContentType("text/html");
+		
+		
+		 int num1;
+		 int num2;
+		 int num3;
+		 
+		 num1 = 4;
+		 num2 = 3;
+		 num3 = 7;
+		
+		 int result = (num1 + num2) * num3;
+		 resp.getWriter().println("Result of " + "("+num1 + "+" + num2 +") *"+ num3+ "="+ result);
+				 
 	}
 }
