584036bf8055f38ab9bea330d1d787cce9efd17e
asaf mashiah <asaf222@gmail.com>
2015-04-23 17:35:42 +0300

    targil 4

---
+++ src/com/algoTrader/javacourse/Stock.java
@@ -0,0 +1,50 @@
+package com.algoTrader.javacourse;
+
+public class Stock {
+	
+	private String symbol;
+	private float ask;
+	private float bid;
+	private java.util.Date date;
+	
+	public Stock(String symbol,float ask,float bid,java.util.Date date){
+		this.symbol = symbol;
+		this.ask = ask;
+		this.bid = bid;
+		this.date = date;
+	}
+	public String getSymbol() {
+		return symbol;
+	}	
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+	public float getAsk() {
+		return ask; 
+	}
+	public void setAsk(float ask) {
+		this.ask = ask;
+	}
+	public float getBid() {
+		return bid;
+	}
+	public void setBid(float bid) {
+		this.bid = bid;
+	}
+	public java.util.Date getDate() {
+		return date;
+	}
+	public void setDate(java.util.Date date) {
+		this.date = date;
+	}
+	
+	
+	
+	public String getHtmlDescription(){
+		String stockDetail = new String("<b>Stock symbol</b>:"+getSymbol()+" <b>ask</b>:"
+				+getAsk()+" <b>bid</b>:"+getBid()+" <b>date</b>:"+getDate().getDate()+ "/"
+				+getDate().getMonth()+ "/" + getDate().getYear());
+		return stockDetail;
+	}
+
+}
---
+++ src/com/algoTrader/javacourse/StockDetailsServlet.java
@@ -0,0 +1,40 @@
+package com.algoTrader.javacourse;
+
+import java.io.IOException;
+import java.util.Calendar;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class StockDetailsServlet extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		resp.setContentType("text/html");		
+		
+		java.util.Date date1 = new java.util.Date();
+		java.util.Date date2 = new java.util.Date();
+		java.util.Date date3 = new java.util.Date();
+		
+		Calendar cal = Calendar.getInstance();
+		cal.set(3914,11,15);
+		date1 = cal.getTime();
+		date2 = cal.getTime();
+		date3 = cal.getTime();
+		Stock stock1 = new Stock("PIH",(float) 13.1,(float) 12.4,date1);
+		Stock stock2 = new Stock("AAL",(float)5.78,(float) 5.5,date2);
+		Stock stock3 = new Stock("CAAS",(float)32.2,(float)31.5,date3);
+		
+		
+		resp.getWriter().println(stock1.getHtmlDescription());
+		resp.getWriter().println("<br>");
+		resp.getWriter().println(stock2.getHtmlDescription());
+		resp.getWriter().println("<br>");
+		resp.getWriter().println(stock3.getHtmlDescription());
+	}
+
+}
